34104651f725ca65069c62f3b41354a0d997d3ba
MlgEpicCar <115513939+MlgEpicCar@users.noreply.github.com>
2024-09-16 14:08:31 -0400

    fixed goofy home PC problem

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -39,9 +39,9 @@ public class Player extends Entity{
     }
     public void setDefaultValues() {
         
-        worldX = gp.tileSize * 23;
-        worldY = gp.tileSize * 21;
-        speed = 4;
+        worldX = gp.tileSize * 23; // 23
+        worldY = gp.tileSize * 21; // 21
+        speed = 4; // 4
         direction = "down";
         // isStopped = "yeah";
     }
--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,7 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hello world");
+        System.out.println("ITS ALIVE");
 
         // WINDOW PARAMETERS
         JFrame window = new JFrame();
